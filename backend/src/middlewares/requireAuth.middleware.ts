5c3c6f7ec6f9c2a6f63951994c5ee065b7c52418
Karim EL KAID <karimelkaid.pro@gmail.com>
2026-06-19 09:50:51 +0200

    Revert "chore(backend): switch from bun to pnpm (#14)"
    This reverts commit da4a0bf316ae587946a81fb37f5d3141f01742ae.

--- backend/src/middlewares/requireAuth.middleware.ts
+++
@@ -1,8 +0,0 @@
-import type { NextFunction, Request, Response } from "express";
-
-export function requireAuth(req: Request, res: Response, next: NextFunction) {
-  if (!req.isAuthenticated()) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-  next();
-}
